d6eb4a6cf5917a88539dbfc247cf5b7f09535fcf
Fuat Akdemir <akdemir_fuat@hotmail.com>
2022-10-12 05:37:21 +0300

    Added solidity files

---
+++ contracts/4_Voting.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.13;
+
+contract Voting {
+    
+    struct Vote {
+        uint x;
+        string y;
+        address d;
+    }
+
+
+
+}
+
